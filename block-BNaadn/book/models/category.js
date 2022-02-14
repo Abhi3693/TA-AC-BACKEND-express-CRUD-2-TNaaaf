e54d8a0b8746aae31925c42d56dea57be49f4ebc
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-02-14 09:26:36 +0530

    Added Assignment-3 of Express CRUD with associations

---
+++ block-BNaadn/book/models/category.js
@@ -0,0 +1,11 @@
+// let mongoose = require("mongoose");
+
+// let Schema = mongoose.Schema;
+
+// let categorySchema = new mongoose.Schema({
+//   category:String,
+// },{timestamps:true});
+
+// let Category = mongoose.model("Category", categorySchema);
+
+// module.exports = Category;
